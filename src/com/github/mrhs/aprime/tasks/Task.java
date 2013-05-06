2ec06cecb9f65a58ee726c9a96c9256310d93dfb
Kevin Brown <bass5098@gmail.com>
2013-05-06 12:17:21 -0400

    Started designing the interface for tasks

---
+++ src/com/github/mrhs/aprime/tasks/Task.java
@@ -0,0 +1,9 @@
+package com.github.mrhs.aprime.tasks;
+
+import java.io.Serializable;
+
+public interface Task extends Serializable
+{
+	public int getId();
+	public TaskResult run();
+}
---
+++ src/com/github/mrhs/aprime/tasks/TaskResult.java
@@ -0,0 +1,8 @@
+package com.github.mrhs.aprime.tasks;
+
+import java.io.Serializable;
+
+public interface TaskResult extends Serializable
+{
+	public int getTaskId();
+}
