0227c466ca40f594f1279c760ec6b40231db04d4
Kevin Brown <bass5098@gmail.com>
2013-05-14 12:32:55 -0400

    Added more internal documentation about receiving tasks

--- src/com/github/mrhs/aprime/TaskReceiver.java
+++ src/com/github/mrhs/aprime/TaskReceiver.java
@@ -45,67 +45,128 @@ public class TaskReceiver extends Thread
 			
 			this.socket = new Socket(this.address, this.port);
 			
+			// GET THE STREAM USED FOR READING THE INCOMING FILE
+			
 			InputStream reader = this.socket.getInputStream();
+			
+			// CREATE A WRITER WHICH WILL BE USED TO TEST IF THE SOCKET IS STILL OPEN
+			
 			PrintWriter writer = new PrintWriter(this.socket.getOutputStream(), true);
 			
+			// CREATE THE FILE WITH THE PROVIDED CLASS NAME IN THE CURRENT DIRECTORY
+			
 			File file = new File(this.className + ".java");
+			
+			// INITIALIZE THE BUFFERS USED FOR WRITING THE DATA TO THE FILE
+			
 			FileOutputStream fileOutput = new FileOutputStream(file);
 			BufferedOutputStream buffOut = new BufferedOutputStream(fileOutput);
 			
 			System.out.println("Stream opened");
 			
+			// CONTINUOUSLY LOOP UNTIL THE SOCKET IS CLOSED
+			// THE WRITER WILL GIVE AN ERROR WHEN IT CAN NO LONGER WRITE TO THE SOCKET
+			// THERE IS NO OTHER EFFICIENT WAY OF DETECTING WHEN IT HAS CLOSED
+			
 			while (!writer.checkError())
 			{
+				// CREATE A 512-BYTE BUFFER THAT WILL RECEIVE THE FILE "CHUNKS"
+				
 				byte[] buffer = new byte[512];
 				
 				reader.read(buffer);
 				
+				// CHECK ALL OF THE INCOMING BYTES AND WRITE THEM TO THE FILE
+				// TODO MAKE THIS MORE EFFICIENT
+				
 				for (int i = 0; i < buffer.length; i++)
 				{
-					// Only write non-null bytes
+					// CHECK EACH BYTE TO MAKE SURE IT IS NOT NULL
+					// JAVA FILES SHOULD NOT CONTAIN NULL BYTES, AND THEY MARK THE END OF THE FILE
 					
 					if (buffer[i] != 0)
 					{
+						// WRITE THE SINGLE VALID BYTE TO THE FILE
+						
 						buffOut.write(buffer[i]);
 					}
 				}
 				
+				// WRITE TO THE SOCKET TO TEST IF IT HAS BEEN CLOSED OR NOT
+				
 				writer.println("Test for close");
 				System.out.println("Received buffer: " + writer.checkError());
 			}
+			
+			// CLOSE THE FILE STREAMS AFTER IT HAS BEEN RECEIVED
 			
 			buffOut.close();
 			fileOutput.close();
 			
 			System.out.println("Stream closed");
 			
+			// INITIALIZE THE JAVA COMPILER AND COMPILE THE DOWNLOADED FILE
+			
 			JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
 			compiler.run(null, null, null, file.getPath());
 			
+			// GET THE DIRECTORY CONTAINING THE FILE
+			
 			File dir = file.getCanonicalFile().getParentFile();
+			
+			// CREATE A NEW CLASS LOADER FOR THE DIRECTORY THAT CONTAINS THE FILE
 			
 			URLClassLoader classLoader = new URLClassLoader(new URL[] { dir.toURI().toURL() });
 			
+			// GET THE NEW CLASS BASED ON THE FULL NAME OF THE CLASS
+			// THIS MUST INCLUDE THE FULL PACKAGE NAME AND CLASS NAME
+			// AND THIS ONLY GRABS CLASSES WHICH HAVE BEEN DOWNLOADED
+			
 			Class<?> cls = Class.forName(this.packageName + "." + this.className, true, classLoader);
 			
+			// CREATE THE NEW INSTANCE OF THE TASK
+			
 			Task task = (Task) cls.newInstance();
+			
+			// FIRE THE taskReceiveFinished HANDLERS WITH THE NEW TASK
 			
 			this.fireReceiveFinished(task);
 		}
 		catch (IOException | ClassNotFoundException | InstantiationException | IllegalAccessException e)
 		{
-			// TODO Auto-generated catch block
+			// TODO HANDLE ERRORS WHEN DOWNLOADING OR COMPILING
+			
 			e.printStackTrace();
 		}
 	}
 	
+	/**
+	 * Adds a TaskListener that will be used when firing events.
+	 * 
+	 * @param listener
+	 *        The TaskListener to be registered.
+	 */
+	
 	public void addListener(TaskListener listener)
 	{
+		// ADD THE GIVEN LISTENER TO THE LIST THAT WILL BE USED FOR FIRING THEM
+		
 		this.listeners.add(listener);
 	}
 	
+	/**
+	 * Fires the taskReceiveFinished() method on all registered handlers.
+	 * Provides the task that was successfully compiled and allows for it to
+	 * be registered internally.
+	 * 
+	 * @param task
+	 * 		  The task that was successfully downloaded and compiled.
+	 */
+	
 	private void fireReceiveFinished(Task task)
 	{
+		// FIRE THE METHOD ON EACH LISTENER
+		
 		for (TaskListener listener : this.listeners)
 		{
 			listener.taskReceiveFinished(task);
