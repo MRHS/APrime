13f49751aa40f0041be00718457f714814bcd673
Spencer Kocot <spencerkocot@gmail.com>
2013-05-22 12:31:28 -0300

    Update Task.java

--- src/com/github/mrhs/aprime/tasks/Task.java
+++ src/com/github/mrhs/aprime/tasks/Task.java
@@ -4,6 +4,9 @@ import java.io.Serializable;
 
 public interface Task extends Serializable
 {
+	//Returns the id of the task
 	public int getId();
+	
+	//The task run method
 	public TaskResult run();
 }
